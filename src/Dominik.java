a5d01e59af80c650e14e89479b612631aab31f28
Dominik Wagner <dominik-wagner@live.de>
2013-08-16 20:57:40 +0200

    help

---
+++ src/Dominik.java
@@ -0,0 +1,12 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dominik Wagner
+ * Date: 16.08.13
+ * Time: 20:50
+ * To change this template use File | Settings | File Templates.
+ */
+public class Dominik {
+    public static void main(String[] args) {
+        System.out.println("Dominik");
+    }
+}
---
+++ src/Wagner.java
@@ -0,0 +1,13 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: Dominik Wagner
+ * Date: 16.08.13
+ * Time: 20:54
+ * To change this template use File | Settings | File Templates.
+ */
+public class Wagner {
+    public static void main (String[] args){
+        System.out.println("Dominik");
+    }
+
+}
